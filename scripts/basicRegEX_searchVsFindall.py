bddfc5c1388ff0cd8deda9dab4821ab8a6bf5fa0
Anthony Bendas <anthony.bendas@gmail.com>
2018-04-23 21:48:13 -0600

    comparing RegEX search vs findall methods

---
+++ scripts/basicRegEX_searchVsFindall.py
@@ -0,0 +1,10 @@
+# findall vs search methods
+
+phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+mo = phoneNumRegex.search('Cell: 415-555-9999 Work: 212-555-0000')
+print(mo.group()) # return 415-555-999
+
+
+phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+mo = phoneNumRegex.findall('Cell: 415-555-9999 Work: 212-555-0000')
+# return '415-555-9999', '212-555-0000'
