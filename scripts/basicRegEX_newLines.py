ac6d8071b441953cd79331bd65a73a56aa7f3ec3
Anthony Bendas <anthony@Anthonys-MacBook-Pro.local>
2018-04-28 22:03:54 -0600

    RegEX new line and re.DOTALL practice

---
+++ scripts/basicRegEX_newLines.py
@@ -0,0 +1,9 @@
+import re
+
+noNewLineRegex = re.compile('.*')
+noNewLineRegex.search('Serve the public trust.\nProtect the innocent.\nUphold the law').group()
+# Serve the public trust.
+
+newLineRegex = re.compile('.*', re.DOTALL)
+newLineRegex.search('Serve the public trust.\nProtect the innocent.\nUphold the law').group()
+# Serve the public trust.\nProtect the innocent.\nUphold the law
